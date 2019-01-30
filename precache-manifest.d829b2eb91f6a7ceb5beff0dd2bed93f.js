5dd48140227b68ebb43e3f84097de1ba7869c1a2
148921 <comtruice@gmail.com>
2019-01-30 15:46:26 +0100

    Updates

---
+++ precache-manifest.d829b2eb91f6a7ceb5beff0dd2bed93f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/148921/ReactWorkshopExperis/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1988bc4ac14614433721",
+    "url": "/148921/ReactWorkshopExperis/static/js/runtime~main.1988bc4a.js"
+  },
+  {
+    "revision": "77cb533a67cba055c791",
+    "url": "/148921/ReactWorkshopExperis/static/js/main.77cb533a.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/148921/ReactWorkshopExperis/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "77cb533a67cba055c791",
+    "url": "/148921/ReactWorkshopExperis/static/css/main.25e3a626.chunk.css"
+  },
+  {
+    "revision": "b99bfab8d672b04dae32dbc1b209685c",
+    "url": "/148921/ReactWorkshopExperis/index.html"
+  }
+];
